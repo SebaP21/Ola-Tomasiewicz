2a2ccb6123aba9d41e14e15af2169f692fe9a260
Sebastian <sebastianporttka@gmail.com>
2024-11-01 21:45:07 +0100

    next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,17 @@ import "./globals.css";
 import NavMobile from "@/components/NavMobile/NavMobile";
 import CookieConsentBanner from "@/components/CookieConsent/CookieConsentBanner";
 import Footer from "@/components/Footer/Footer";
-import Head from "next/head";
+import { Didact_Gothic } from "next/font/google";
 
 export const metadata: Metadata = {
 	title: "Ola Tomasiewicz Makeup Artist",
 	description: "Projekt",
 };
+
+const didact = Didact_Gothic({
+	subsets: ["latin"],
+	weight: "400",
+});
 
 export default function RootLayout({
 	children,
@@ -17,14 +22,14 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='pl'>
-			<Head>
+			<head>
 				
 				<link
 					href='https://fonts.googleapis.com/css2?family=Didact+Gothic:wght@400&family=Oswald:wght@400&display=swap'
 					rel='stylesheet'
 				/>
-			</Head>
-			<body>
+			</head>
+			<body className={didact.className}>
 				<NavMobile />
 				{children}
 				<Footer/>
