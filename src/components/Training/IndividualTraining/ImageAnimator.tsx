ca0f193fda70ed6846bf6e875688a3df3f9ac4d2
Sebastian <sebastianporttka@gmail.com>
2024-11-05 01:13:57 +0100

    last chance

--- src/components/Training/IndividualTraining/ImageAnimator.tsx
+++ src/components/Training/IndividualTraining/ImageAnimator.tsx
@@ -3,140 +3,153 @@ import React, { useEffect, useState, useRef, useCallback } from "react";
 import Image from "next/image";
 
 type MediaItem = {
-  __typename?: "MediaItem";
-  altText?: string | null;
-  mediaItemUrl?: string | null;
-  slug?: string | null;
+	__typename?: "MediaItem";
+	altText?: string | null;
+	mediaItemUrl?: string | null;
+	slug?: string | null;
 };
 
 type AnimatedGalleryProps = {
-  gallery: MediaItem[] | undefined;
+	gallery: MediaItem[] | undefined;
 };
 
 const AnimatedGallery: React.FC<AnimatedGalleryProps> = ({ gallery }) => {
-  const [shadowIndex, setShadowIndex] = useState<number>(0);
-  const [visibleIndexes, setVisibleIndexes] = useState<Set<number>>(new Set());
-  const imageRefs = useRef<(HTMLDivElement | null)[]>([]);
-  
-  // Używamy tablicy do przechowywania stanu kliknięcia dla każdego zdjęcia
-  const [clickedStates, setClickedStates] = useState<boolean[]>([]);
+	const [shadowIndex, setShadowIndex] = useState<number>(0);
+	const [visibleIndexes, setVisibleIndexes] = useState<Set<number>>(new Set());
+	const [hoverStates, setHoverStates] = useState<boolean[]>([]);
+	const imageRefs = useRef<(HTMLDivElement | null)[]>([]);
 
-  // Zmieniamy cień co 2 sekundy
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setShadowIndex((prevIndex) => (prevIndex + 1) % 3);
-    }, 2000);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setShadowIndex((prevIndex) => (prevIndex + 1) % 3);
+		}, 2000);
 
-    return () => clearInterval(interval);
-  }, []);
+		return () => clearInterval(interval);
+	}, []);
 
-  // Funkcja do obserwowania widoczności elementów
-  const observeImages = useCallback(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          const index = Number(entry.target.getAttribute("data-index"));
-          if (entry.isIntersecting) {
-            setVisibleIndexes((prev) => new Set(prev).add(index));
-            observer.unobserve(entry.target); // Przestajemy obserwować widoczny element
-          }
-        });
-      },
-      { threshold: 0.5 }
-    );
+	const observeImages = useCallback(() => {
+		const observer = new IntersectionObserver(
+			(entries) => {
+				entries.forEach((entry) => {
+					const index = Number(entry.target.getAttribute("data-index"));
+					if (entry.isIntersecting) {
+						setVisibleIndexes((prev) => new Set(prev).add(index));
+						observer.unobserve(entry.target);
+					}
+				});
+			},
+			{ threshold: 0.5 }
+		);
 
-    imageRefs.current.forEach((ref) => {
-      if (ref) observer.observe(ref);
-    });
+		imageRefs.current.forEach((ref) => {
+			if (ref) observer.observe(ref);
+		});
 
-    return () => observer.disconnect();
-  }, []);
+		return () => observer.disconnect();
+	}, []);
 
-  // Uruchamiamy obserwowanie zdjęć po załadowaniu galerii
-  useEffect(() => {
-    if (gallery && gallery.length > 0) {
-      observeImages();
-    }
-  }, [gallery, observeImages]);
+	useEffect(() => {
+		if (gallery && gallery.length > 0) {
+			observeImages();
+			setHoverStates(Array(gallery.length).fill(false));
+		}
+	}, [gallery, observeImages]);
 
-  // Aktualizujemy stan kliknięcia, gdy galeria jest załadowana
-  useEffect(() => {
-    if (gallery) {
-      setClickedStates(Array(gallery.length).fill(false)); // Inicjalizujemy stan kliknięcia
-    }
-  }, [gallery]);
+	const handleMouseEnter = (index: number) => {
+		setHoverStates((prev) => {
+			const newHoverStates = [...prev];
+			newHoverStates[index] = true;
+			return newHoverStates;
+		});
+	};
 
-  const handleClick = (index: number) => {
-    setClickedStates((prev) => {
-      const newClickedStates = [...prev];
-      newClickedStates[index] = !newClickedStates[index]; // Przełączamy stan dla klikniętego zdjęcia
-      return newClickedStates;
-    });
-  };
+	const handleMouseLeave = (index: number) => {
+		setHoverStates((prev) => {
+			const newHoverStates = [...prev];
+			newHoverStates[index] = false;
+			return newHoverStates;
+		});
+	};
 
-  return (
-    <div className='grid grid-cols-1 gap-1 lg:grid-cols-2'>
-      {gallery?.map((picture, index) => (
-        <div
-          key={index}
-          className={`overflow-hidden relative transition-opacity duration-1000 ${
-            visibleIndexes.has(index) ? "opacity-100" : "opacity-0"
-          }`}
-          data-index={index}
-          ref={(el) => { imageRefs.current[index] = el }}
-          onTouchStart={() => handleClick(index)} // Obsługa kliknięcia na urządzeniach dotykowych
-          onClick={() => handleClick(index)} // Obsługa kliknięcia
-        >
-          <Image
-            src={picture.mediaItemUrl || ""}
-            alt={picture.altText || ""}
-            width={1200}
-            height={1200}
-            className='w-full h-full object-cover'
-          />
+	const handleImageClick = (index: number) => {
+		setHoverStates((prev) => {
+			const newHoverStates = [...prev];
+			newHoverStates[index] = !newHoverStates[index];
+			return newHoverStates;
+		});
+	};
 
-          {/* Animowany cień, tylko jeśli nie ma kliknięcia */}
-          <div
-            className={`absolute inset-0 shadow-layer ${
-              shadowIndex === 0 && !clickedStates[index] ? "opacity-100" : "opacity-0"
-            } shadow-right`}
-          ></div>
-          <div
-            className={`absolute inset-0 shadow-layer ${
-              shadowIndex === 1 && !clickedStates[index] ? "opacity-100" : "opacity-0"
-            } shadow-left`}
-          ></div>
-          <div
-            className={`absolute inset-0 shadow-layer ${
-              shadowIndex === 2 && !clickedStates[index] ? "opacity-100" : "opacity-0"
-            } no-shadow`}
-          ></div>
-        </div>
-      ))}
-      <style jsx>{`
-        .shadow-layer {
-          transition: opacity 1.5s ease;
-        }
-        .shadow-right {
-          background: linear-gradient(
-            to left,
-            rgba(0, 0, 0, 0.85) 45%,
-            transparent 100%
-          );
-        }
-        .shadow-left {
-          background: linear-gradient(
-            to right,
-            rgba(0, 0, 0, 0.85) 45%,
-            transparent 100%
-          );
-        }
-        .no-shadow {
-          background: none;
-        }
-      `}</style>
-    </div>
-  );
+	return (
+		<div className='grid grid-cols-1 gap-1 lg:grid-cols-2'>
+			{gallery?.map((picture, index) => (
+				<div
+					key={index}
+					className={`overflow-hidden relative transition-opacity duration-1000 ${
+						visibleIndexes.has(index) ? "opacity-100" : "opacity-0"
+					}`}
+					data-index={index}
+					ref={(el) => {
+						imageRefs.current[index] = el;
+					}}
+					onMouseEnter={() => handleMouseEnter(index)}
+					onMouseLeave={() => handleMouseLeave(index)}
+					onClick={() => handleImageClick(index)}
+				>
+					<Image
+						src={picture.mediaItemUrl || ""}
+						alt={picture.altText || ""}
+						width={1200}
+						height={1200}
+						className='w-full h-full object-cover'
+					/>
+
+					<div
+						className={`absolute inset-0 shadow-layer ${
+							shadowIndex === 0 && !hoverStates[index]
+								? "opacity-100"
+								: "opacity-0"
+						} shadow-right`}
+					></div>
+					<div
+						className={`absolute inset-0 shadow-layer ${
+							shadowIndex === 1 && !hoverStates[index]
+								? "opacity-100"
+								: "opacity-0"
+						} shadow-left`}
+					></div>
+					<div
+						className={`absolute inset-0 shadow-layer ${
+							shadowIndex === 2 && !hoverStates[index]
+								? "opacity-100"
+								: "opacity-0"
+						} no-shadow`}
+					></div>
+				</div>
+			))}
+			<style jsx>{`
+				.shadow-layer {
+					transition: opacity 1.5s ease;
+				}
+				.shadow-right {
+					background: linear-gradient(
+						to left,
+						rgba(0, 0, 0, 0.85) 45%,
+						transparent 100%
+					);
+				}
+				.shadow-left {
+					background: linear-gradient(
+						to right,
+						rgba(0, 0, 0, 0.85) 45%,
+						transparent 100%
+					);
+				}
+				.no-shadow {
+					background: none;
+				}
+			`}</style>
+		</div>
+	);
 };
 
 export default AnimatedGallery;
