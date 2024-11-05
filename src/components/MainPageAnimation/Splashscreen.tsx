ddf62a26839349d67a7e8f007d9b13400267f967
Sebastian <sebastianporttka@gmail.com>
2024-11-05 05:14:36 +0100

    another animation

--- src/components/MainPageAnimation/Splashscreen.tsx
+++ src/components/MainPageAnimation/Splashscreen.tsx
@@ -3,79 +3,118 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import logo from "../../assets/pictures/ZNAK WODNY CZARNY_Obszar roboczy 1.png"; // Upewnij się, że ścieżka jest poprawna
 
-
 const AnimationMain = () => {
+	const [animationState, setAnimationState] = useState("start");
 	const [isVisible, setIsVisible] = useState(true);
-	const [isFadingOut, setIsFadingOut] = useState(false);
 
 	useEffect(() => {
 		const timer = setTimeout(() => {
-			setIsFadingOut(true); // Rozpocznij efekt zanikania
-			// Kolejny timer, aby całkowicie usunąć komponent po zaniku
-			setTimeout(() => setIsVisible(false), 300); // Zmniejszone do 0.3s dla efektu zanikania
-		}, 2000); // 1s dla animacji linii + 0.5s dla animacji tła
+			setAnimationState("end");
+		}, 1400);
 
-		return () => clearTimeout(timer); // Czyszczenie timera przy demontażu komponentu
+		const visibilityTimer = setTimeout(() => {
+			setIsVisible(false);
+		}, 2000);
+
+		return () => {
+			clearTimeout(timer);
+			clearTimeout(visibilityTimer);
+		};
 	}, []);
 
-	if (!isVisible) return null; // Wyłącz komponent po zakończeniu zanikania
+	if (!isVisible) return null;
 
 	return (
-		<div
-			className={`min-w-full min-h-[100svh] top-0 left-0 sticky bg-white flex justify-center items-center z-50 overflow-hidden transition-opacity duration-500 ${
-				isFadingOut ? "opacity-0" : "opacity-100"
-			}`}
-		>
-			<div
-				className={`relative min-h-[100svh] flex justify-center items-center transition-transform duration-500`}
-			>
-				<Image
-					src={logo}
-					alt={"logo"}
-					width={300}
-					height={300}
-				/>
+		<>
+			<div className='fixed h-[100svh] w-full z-50'>
+				<div
+					className={`w-1/2 h-full absolute top-0 left-0 z-50 bg-white ${
+						animationState === "end" ? "animate-fade-out-left" : ""
+					}`}
+				>
+					<div className='min-w-[1px] right-0 bottom-0 h-[1px] absolute bg-black  animate-height'></div>
+					<div className='min-w-[0px] right-0 bottom-0 h-[100svh] absolute bg-black  animate-width'></div>
+				</div>
 
-				
-				<div className='absolute bottom-0 left-1/2 transform -translate-x-1/2 w-[2px] bg-accent animate-line'></div>
+				<div
+					className={`w-1/2 h-full absolute top-0 right-0 z-50 bg-white  ${
+						animationState === "end" ? "animate-fade-out-right" : ""
+					}`}
+				>
+					<div className='min-w-[1px] left-0 h-[1px] absolute bg-black  animate-height '></div>
+					<div className='min-w-[0px] left-0 bottom-0 h-[100svh] absolute bg-black  animate-width'></div>
+				</div>
+
+				<div
+					className={`absolute inset-0 flex items-center justify-center z-50 transition-opacity duration-500 ${
+						animationState === "end" ? "opacity-0" : ""
+					}`}
+				>
+					<Image
+						src={logo}
+						alt='Logo'
+						width={350}
+						height={350}
+					/>
+				</div>
 			</div>
-
-			
-			<div className='absolute inset-0 bg-black animate-background opacity-0'></div>
-
 			<style jsx>{`
-				/* Animacja linii przechodzącej od dołu do góry */
-				@keyframes line {
+				@keyframes fadeOutLeft {
 					0% {
-						height: 0;
-					}
-					100% {
-						height: 100%;
-					}
-				}
-
-				/* Animacja rozszerzającego się tła */
-				@keyframes background {
-					0% {
-						transform: scaleX(0);
-						opacity: 0;
-					}
-					100% {
-						transform: scaleX(1);
+						transform: translateX(0);
 						opacity: 1;
 					}
+					100% {
+						transform: translateX(-100%);
+						opacity: 0;
+					}
 				}
 
-				/* Klasy Tailwind do animacji */
-				.animate-line {
-					animation: line 0.75s ease-in forwards; /* Skrócone do 0.75s */
+				@keyframes fadeOutRight {
+					0% {
+						transform: translateX(0);
+						opacity: 1;
+					}
+					100% {
+						transform: translateX(100%);
+						opacity: 0;
+					}
 				}
-				.animate-background {
-					animation: background 0.5s ease-in 0.75s forwards; /* Skrócone do 0.5s z opóźnieniem 0.75s */
-					transform-origin: center;
+				@keyframes width {
+					0% {
+						width: 0;
+					}
+					100% {
+						width: 100%;
+					}
+				}
+				@keyframes height {
+					0% {
+						height: 1px;
+					}
+					100% {
+						height: 100svh;
+					}
+				}
+
+				.animate-height {
+					animation: height 0.5s forwards;
+				}
+
+				.animate-width {
+					animation: width 0.7s forwards;
+					animation-delay: 0.7s;
+				}
+
+				.animate-fade-out-left {
+					animation: fadeOutLeft 0.7s forwards;
+				}
+
+				.animate-fade-out-right {
+					animation: fadeOutRight 0.7s forwards;
 				}
 			`}</style>
-		</div>
+		</>
 	);
 };
 
